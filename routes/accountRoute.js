a39b398c231e22083d93af6a4a316fd57a03482b
Marc MOREL <mmorel@likbe.com>
2012-09-25 01:21:59 +0200

    Style and content update

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,7 +3,7 @@ var accountService = require('../apis/accountService.js');
 exports.login = function(res, user) {
 	accountService.login(user, function(err, result) {
 		if (err) { res.render('error', { error : { message: 'Unable to retrieve user with id : ' + user }}); }
-		else { res.render('homeAuthenticated', {user : result}); }
+		else { res.render('homeAuthenticated', {user : result, layout : 'layoutAuthenticated'}); }
 	});
 }
 
--- static/js/logout.js
+++ static/js/logout.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	$('#logout-action').click(function() {
-		$.post('/user/logout', function() {
-  			$('#result').html('<h2>Logged out !</h2>');
-		});
+		alert('Implement sign out');		
 	});
 });
