c38f14ff71a98dd65027d4f72bcde041df13bac3
Marc MOREL <mmorel@likbe.com>
2012-10-08 07:26:01 +0200

    Authorization Service : improve check for admin role to access to users list

--- apis/authorizationService.js
+++ apis/authorizationService.js
@@ -1,18 +1,32 @@
 var accountService = require("./accountService.js");
+var async = require("async");
 
 extractFeatureKey = function(req) {
 	console.log(req.url);
 	return req.url;
 }
 
+isAdmin = function(item, callback) {
+    console.log(item);
+    if (item.name == 'admin') { callback(true); }
+    else { callback(false); }
+}
+
 exports.ensureSecurity = function(req, res, next) {
 	var featureKey = extractFeatureKey(req);
-	console.log('feature key:'+featureKey);
+    var isAuthorized = false;
     if (req.isAuthenticated()) {
-    	// TODO check for 'admin' role only
-    	if (featureKey == "/users" && req.user.roles.length == 1) {
-    		return next();
+    	if (featureKey == "/users") {
+            async.detect(req.user.roles, isAdmin, function(result) {
+                if (result != null) {
+                    isAuthorized = true;
+                }
+            });
     	}
+        else {
+            isAuthorized = true;
+        }
  	}
-    return res.redirect('/user/login');
+    if (isAuthorized) { return next(); }
+    else { return res.redirect('/user/login'); }
 }
